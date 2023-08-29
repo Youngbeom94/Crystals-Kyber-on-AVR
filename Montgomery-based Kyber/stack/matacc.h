84ecabb0d120d1dd5f7c5fb8306b636989497064
YoungBeom Kim <darania@Kookmin.ac.kr>
2023-08-29 19:50:27 +0900

    first commit

--- Montgomery-based Kyber/stack/matacc.h
+++
@@ -1,9 +0,0 @@
-#ifndef MATACC_H
-#define MATACC_H
-#include "poly.h"
-#include "polyvec.h"
-#include "symmetric.h"
-
-void matacc(poly* res, polyvec* src, unsigned char i, const unsigned char* seed, int transposed);
-
-#endif
